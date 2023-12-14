72079be2fe5e951bbf06a5eae10a28365488b299
ricardo <ricardofg2000@gmail.com>
2023-12-15 00:14:21 +0100

    subida proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Usuarios';
+  mensaje = '';
+  registrado = false;
+  nombre: String = '';
+  apellido: String = '';
+  cargo: string = '';
+  entradas: any[];
+
+  constructor() {
+    this.entradas = [
+      { titulo: 'Python, ejemplo de entrada 1' },
+      { titulo: 'Java, ejemplo de entrada 2' },
+      { titulo: 'JavaScript, ejemplo de entrada 3' },
+      { titulo: 'SpringBoot, ejemplo de entrada 4' },
+      { titulo: 'QSyo, ejemplo de entrada 5' },
+    ];
+  }
+
+  registrarUsuario() {
+    this.registrado = true;
+
+    this.mensaje = 'Usuario registrado con exito ';
+  }
 }
